ddac5a9c26dae3637c0e425a58787d5f8bdbeb9c
Matthew Anderson <manderson@onehub.com>
2012-07-09 23:05:13 -0700

    Dumping in prototype files and using Sprockets to include JS.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,4 +1,7 @@
-//= require_tree .
+//= require "jquery-1.7.2"
+//= require "jquery.easing.1.3"
+//= require "jquery.mobile.customized.min"
+//= require "jquery.camera"
 
 jQuery(function(){
   jQuery('#onehub .slides').camera({
