8cb1e8c4f7d434930ffbcbb0470863828d5827aa
Matthew Anderson <manderson@onehub.com>
2012-07-22 18:46:44 -0700

    Smooth scrolling between the sections.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,7 +1,14 @@
 //= require "jquery-1.7.2"
+//= require "jquery.easing-1.3"
+//= require "jquery.scrollTo"
 //= require "jquery.flexslider"
 
 $(function() {
+  $('.work a').click(function() {
+    var target = $(this).attr('href');
+    $.scrollTo(target, 500, {easing: 'easeOutExpo'});
+  });
+
   $('.toggle-work').click(function() {
     $('.work').slideToggle('fast');
   });
