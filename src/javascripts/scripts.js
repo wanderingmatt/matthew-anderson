b9888edaac0bf8b4848a45850879cf0a54542b31
Matthew Anderson <matt@wanderingmatt.com>
2017-05-06 23:31:35 -0700

    Better dots and styling the asides slightly different.

--- src/javascripts/scripts.js
+++ src/javascripts/scripts.js
@@ -1,7 +1,19 @@
 $(document).ready(function(){
   $('.slides').slick({
+    arrows: false,
     dots: true,
     lazyLoad: 'progressive',
-    mobileFirst: true
+    // mobileFirst: true,
+    // responsive: [
+    //   {
+    //     breakpoint: 767,
+    //     settings: {
+    //       slidesToShow: 3,
+    //       slidesToScroll: 3,
+    //       infinite: true,
+    //       dots: true
+    //     }
+    //   }
+    // ]
   });
 });
